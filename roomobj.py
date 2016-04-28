6d7d78e15edf09671876d3c2b5ed0dd388a95e8d
bernardsoulier <albertsun05@gmail.com>
2016-04-28 05:53:29 -0400

    changed Room1 to roomlist[0]
    roomlist is planned as a list of room objects.  still need to implement a working command function.  would start with directions as this should be easiest to implement.

--- roomobj.py
+++ roomobj.py
@@ -24,16 +24,17 @@ class Room:
 """ main below, will likely go to it's own file in future, but for now this is the temp main """
 
 def start():
-    print("Welcome to Fun House, where anything could happen...")
-    room1 = Room("You are in an empty room, painted in a pleasant indigo color.", [], {"n":"room2"})
-    roomloop(room1)
+    print("Welcome to the Mystery House, where anything could happen...")
+    roomlist = []
+    roomlist.append(Room("You are in an empty room, painted in a pleasant indigo color.", [], {"n":"room2"}))
+    roomloop(roomlist[0])
 
 def roomloop(currentroom):
     currentroom.describeroom()
     currentroom.exitsfromroom()
     command = input("What would you like to do?")
     if command in currentroom.directionsoutdict:
-        currentroom = room1.directionsoutdict[command]
+        currentroom = currentroom.directionsoutdict[command] #this is a placeholder - not fully implemented
         print(currentroom)
 
 if __name__ == '__main__':
